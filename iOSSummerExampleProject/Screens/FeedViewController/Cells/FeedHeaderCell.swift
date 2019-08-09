e483056c76d150ee8276eb3b5aec46c39cfcf38d
xcode <dev.fixique@gmail.com>
2019-08-09 15:32:54 +0300

    Лекция 5

---
+++ iOSSummerExampleProject/Screens/FeedViewController/Cells/FeedHeaderCell.swift
@@ -0,0 +1,50 @@
+//
+//  FeedHeaderCell.swift
+//  iOSSummerExampleProject
+//
+//  Created by xcode on 09.08.2019.
+//  Copyright © 2019 Surf. All rights reserved.
+//
+
+import UIKit
+
+class FeedHeaderCell: UITableViewCell {
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var headerImageView: UIImageView!
+    @IBOutlet weak var overlayView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    
+    // MARK: - UITableViewCell
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        configureHeaderImage()
+        configureLabels()
+    }
+    
+    // MARK: - Configuration
+    
+    func configureHeaderImage() {
+        headerImageView.contentMode = .scaleAspectFill
+        headerImageView.image = UIImage(named: "headerImage")
+        overlayView.backgroundColor = UIColor.black.withAlphaComponent(0.3)
+    }
+
+    func configureLabels() {
+        titleLabel.font = UIFont.systemFont(ofSize: 32.0, weight: .semibold)
+        titleLabel.textColor = UIColor.white
+        titleLabel.numberOfLines = 3
+        titleLabel.textAlignment = .center
+        titleLabel.text = "Photos for everyone"
+        
+        authorLabel.font = UIFont.systemFont(ofSize: 12.0, weight: .regular)
+        authorLabel.textColor = UIColor(red: 189.0/255.0, green: 189.0/255.0, blue: 189.0/255.0, alpha: 1)
+        authorLabel.numberOfLines = 1
+        authorLabel.textAlignment = .center
+        authorLabel.text = "Photo by Marcelo Cidrack"
+    }
+    
+}
